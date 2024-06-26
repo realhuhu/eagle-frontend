4f2ccddceb4750e7023189d903ff47c2570ecc00
realhuhu <2661467107@qq.com>
2024-06-26 23:38:39 +0800

    tip

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -4,6 +4,8 @@
       <div class="text-[36px] sm:text-[64px] md:text-[100px] text-[#4ebaee] drop-shadow-2xl">
         IYING.LOVE
       </div>
+      <div class="text-red-500">本站为旧版本，将于7月15日下线</div>
+      <div>新版本：<a href="https://iying.love" class="text-[#4ebaee]">iying.love</a></div>
 
       <InfoTable/>
     </div>
